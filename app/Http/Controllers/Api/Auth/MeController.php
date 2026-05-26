ec27af86c306bbfb10a2c1d2b45e6a17d26aab2f
Marcellus <marcelomiller557@gmail.com>
2026-05-26 01:25:25 -0300

    feat: Me endpoint

--- app/Http/Controllers/Api/Auth/MeController.php
+++ app/Http/Controllers/Api/Auth/MeController.php
@@ -7,5 +7,18 @@ use Illuminate\Http\Request;
 
 class MeController extends Controller
 {
-    //
+    public function show(Request $request)
+    {
+        $user = $request->user();
+
+        return response()->json([
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'avatar_url' => $user->avatar_url,
+            ],
+            'roles' => $user->getRoleNames(),
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\MeController;
 use App\Http\Controllers\Api\Auth\RegisterController;
-use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post("register", [RegisterController::class, "store"]);
@@ -11,5 +11,5 @@ Route::post("login", [LoginController::class, "store"]);
 
 Route::middleware("auth:sanctum")->group(function () {
     Route::post("logout", [LogoutController::class, "destroy"]);
-    // Route::get("/me", [UserController::class, "show"]);
+    Route::get("me", [MeController::class, "show"]);
 });
